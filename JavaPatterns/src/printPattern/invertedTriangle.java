3597d64e7a5341ef93868afb7d736a25da1cbd99
Sailee Gokhale <Sailee Gokhale@LAPTOP-5CFUU8KJ>
2019-06-04 17:14:36 +0530

    commit2

--- JavaPatterns/src/printPattern/invertedTriangle.java
+++ JavaPatterns/src/printPattern/invertedTriangle.java
@@ -3,8 +3,12 @@ package printPattern;
 public class invertedTriangle {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int i,j;
+		for(i=1;i<=7;i++) {
+			for(j=1;j<=i;j++) {
+				System.out.print(i+" ");
+			}
+			System.out.println();	
+		}
 	}
-
 }
